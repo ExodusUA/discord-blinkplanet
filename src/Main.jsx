90c389574e772611524dfd42c5c1eda31845d00e
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2023-10-31 00:21:16 +0200

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,29 +5,24 @@ import loading from './loading.svg'
 function Main() {
 
     const urlCode = window.location.href.split('/').pop()
-    const urlParams = new URLSearchParams(window.location.search)
 
     useEffect(() => {
 
-        if (urlParams.get('i') !== null) {
-            const isPrivate = requestAPI.getPassesData(urlCode)
-
-            isPrivate.then((data) => {
-                if (data.isPrivate === true) {
-                    window.location.replace(`http://discord.gg/54G95nUytY`)
-                } else {
-                    window.location.replace(`http://discord.gg/blinkplanet`)
-                }
-
-            }).catch((err) => {
-                console.log(err)
-            })
-        } else {
+        if (urlCode.length > 0) {
             const isPrivate = requestAPI.getPassesData(urlCode)
 
             isPrivate.then((data) => {
 
                 window.location.replace(`https://blink-planet.vercel.app/redirect?code=${urlCode}&private=${data.isPrivate}`)
+
+                /*
+                if (data.isPrivate === true) {
+                    window.location.replace(`http://discord.gg/54G95nUytY`)
+                } else {
+                    window.location.replace(`http://discord.gg/blinkplanet`)
+                }
+                */
+
             }).catch((err) => {
                 console.log(err)
             })
